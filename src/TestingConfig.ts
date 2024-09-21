7712b83acd7cf848216289f293ef53105093a6d9
kbwo <kabaaa1126@gmail.com>
2024-09-21 12:51:06 +0900

    initial commit

---
+++ src/TestingConfig.ts
@@ -0,0 +1,56 @@
+import * as vscode from 'vscode';
+import {
+  LanguageClientOptions,
+  ServerOptions,
+} from 'vscode-languageclient/node';
+
+export class TestingConfig {
+  private config: vscode.WorkspaceConfiguration;
+
+  constructor() {
+    this.config = vscode.workspace.getConfiguration('testing');
+  }
+
+  get isEnabled(): boolean {
+    return this.config.get<boolean>('enable', false);
+  }
+
+  get serverPath(): string | undefined {
+    return this.config.get<string>('server.path');
+  }
+
+  get fileTypes(): string[] {
+    return this.config.get<string[]>('fileTypes', ['rust']);
+  }
+
+  get enableWorkspaceDiagnostics(): boolean {
+    return this.config.get<boolean>('enableWorkspaceDiagnostics', false);
+  }
+
+  get serverOptions(): ServerOptions {
+    return {
+      command: this.serverPath!,
+      args: []
+    };
+  }
+
+  get clientOptions(): LanguageClientOptions {
+    const options: LanguageClientOptions = {
+      documentSelector: this.fileTypes.map(lang => ({ scheme: 'file', language: lang })),
+      synchronize: {
+        fileEvents: vscode.workspace.createFileSystemWatcher(`**/*.{${this.fileTypes.join(',')}}`)
+      }
+    };
+
+    const traceServer = this.config.get<string>('trace.server', 'off');
+    if (traceServer !== 'off') {
+      options.traceOutputChannel = vscode.window.createOutputChannel('Testing Language Server Trace');
+    }
+
+    return options;
+  }
+
+  get adapterCommands(): any {
+    return this.config.get<any>('adapterCommand', {});
+  }
+}
---
+++ src/extension.ts
@@ -0,0 +1,43 @@
+import * as vscode from 'vscode';
+import { LanguageClient } from 'vscode-languageclient/node';
+import { TestingConfig } from './TestingConfig';
+
+let client: LanguageClient;
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  const testingConfig = new TestingConfig();
+
+  // Check if testing is enabled
+  if (!testingConfig.isEnabled) {
+    return;
+  }
+
+  if (!testingConfig.serverPath) {
+    vscode.window.showErrorMessage('Testing Language Server path is not set. Please configure it in the settings.');
+    return;
+  }
+
+  client = new LanguageClient(
+    'testingLanguageServer',
+    'Testing Language Server',
+    testingConfig.serverOptions,
+    testingConfig.clientOptions
+  );
+
+  client.start();
+
+  // Enable workspace diagnostics if configured
+  if (testingConfig.enableWorkspaceDiagnostics) {
+    // Implement workspace diagnostics here
+  }
+}
+
+// This method is called when your extension is deactivated
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
